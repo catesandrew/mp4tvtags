91297f2c77652bf54b6cfef3b56dfc0490f0d5f2
Andrew Cates <catesandrew@gmail.com>
2011-04-27 22:57:41 -0700

    removed refs to MP4Tagger, opting for mp4v2/mp4tags

--- mp4tvtags.py
+++ mp4tvtags.py
@@ -40,7 +40,7 @@ class Program:
             print "Connecting to the TVDB... "
         #end if verbose
         self.tvdb = Tvdb(debug = opts.debug, interactive = opts.interactive, banners = True)
-        self.mp4tagger = os.path.dirname(__file__) + "/MP4Tagger"
+        self.mp4tagger = "mp4tags"
         self.dirPath = dirPath
     #end def __init__
 #end class Program      
@@ -98,11 +98,18 @@ class Episode:
         
             writersUnsplit = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'writer')
             self.writers = writersUnsplit.split('|')
+            
+            self.imdbId = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'imdb_id')
+            self.id = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'id')
         else:
             #get information for only first episode in multiepisode listing
             self.firstAired = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'firstaired') + "T09:00:00Z"
             self.productionCode = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'productioncode')
             self.overview = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'overview')
+            
+            self.imdbId = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'imdb_id')
+            self.id = getEpisodeSpecificInfo(verbose, program, series, self.episodeNumbers[0], 'id')
+            
             
             self.episodeName = ""
             self.directors = []
@@ -195,27 +202,9 @@ def correctFileName(opts, program, series, episode):
 
 def tagFile(opts, program, series, episode):
     """docstring for tagFile"""
-    if not opts.forcetagging:
-        #check if file has already been tagged
-        alreadyTaggedCmd = "\"%s\" -i \"%s/%s\" -t" % (program.mp4tagger, program.dirPath, episode.fileName)
-        #cmd = "\"" + program.mp4tagger + "\" -i \"" + program.dirPath + "/" + episode.fileName.encode("utf-8") + "\"" + " -t"
-        if opts.debug:
-            print "!!AlreadyTagged command: %s" % alreadyTaggedCmd
-        #end if debug
-        existingTagsUnsplit = os.popen(alreadyTaggedCmd).read()
-        existingTags = existingTagsUnsplit.split('\r')
-        for line in existingTags:
-            if line.count("tagged by mp4tvtags"):
-                if opts.verbose:
-                    print "  %s already tagged" % episode.fileName
-                #end if verbose
-                return
-            #end if line.count
-        #end for line
-    #end if opts.forcetagging
     #setup tags for the MP4Tagger function
     if series.artworkFileName != "":
-        addArtwork = " --artwork \"%s/%s\"" % (program.dirPath, series.artworkFileName) #the file we downloaded earlier
+        addArtwork = " -picture \"%s/%s\"" % (program.dirPath, series.artworkFileName) #the file we downloaded earlier
     else:
         addArtwork = ""
     #end if series.artworkFileName != ""
@@ -225,36 +214,45 @@ def tagFile(opts, program, series, episode):
         addRating = ""
     #end if series.rating != "":
     
-    addMediaKind = " --media_kind \"TV Show\"" #set type to TV Show
-    addArtist = " --artist \"%s\"" % series.seriesName
-    addName =  " --name \"%s\"" % episode.episodeName
-    addAlbum = " --album \"%s - Season %s\"" % (series.seriesName, series.seasonNumber)
-    addGenre = " --genre \"%s\"" % series.genres[1] #cause first one is an empty string, and genre can only have one entry
-    addAlbumArtist = " --album_artist \"%s\"" % series.seriesName
-    addDescription = " --description \"%s\"" % episode.overview
-    addLongDescription = " --long_description \"%s\"" % episode.longOverview
-    addTVNetwork = " --tv_network \"%s\"" % series.network
-    addTVShowName = " --tv_show \"%s\"" % series.seriesName
-    addTVEpisode = " --tv_episode_id \"%s\"" % episode.productionCode
-    addTVSeasonNum = " --tv_season \"%i\"" % series.seasonNumber
+    addMediaKind = " -type \"TV Show\"" #set type to TV Show
+    addArtist = " -artist \"%s\"" % series.seriesName
+    addName =  " -song \"%s\"" % episode.episodeName
+    addAlbum = " -album \"%s - Season %s\"" % (series.seriesName, series.seasonNumber)
+    addGenre = " -genre \"%s\"" % series.genres[1] #cause first one is an empty string, and genre can only have one entry
+    addAlbumArtist = " -albumartist \"%s\"" % series.seriesName
+    
+    #addDescription = " -description \"%s\"" % episode.overview
+    data = episode.overview
+    shortDescription = (data[:250] + '...') if len(data) > 250 else data
+    addDescription = " -description \"%s\"" % shortDescription  
+    
+    #addLongDescription = " -longdesc \"%s\"" % episode.longOverview
+    data = episode.longOverview
+    addLongDescription = " -longdesc \"%s\"" % episode.longOverview
+    
+    
+    addTVNetwork = " -network \"%s\"" % series.network
+    addTVShowName = " -show \"%s\"" % series.seriesName
+    addTVEpisode = " -episodeid \"%s\"" % episode.productionCode
+    addTVSeasonNum = " -season \"%i\"" % series.seasonNumber
     
     #kept for clarity as it same decision is made
     if episode.singleEpisode:
-        addTVEpisodeNum = " --tv_episode_n \"%i\"" % episode.episodeNumbers[0]
-        addTracknum = " --track_n \"%i\"" % episode.episodeNumbers[0]
+        addTVEpisodeNum = " -episode \"%i\"" % episode.episodeNumbers[0]
+        addTracknum = " -track \"%i\"" % episode.episodeNumbers[0]
     else:
-        addTVEpisodeNum = " --tv_episode_n \"%i\"" % episode.episodeNumbers[0]
-        addTracknum = " --track_n \"%i\"" % episode.episodeNumbers[0]    
+        addTVEpisodeNum = " -episode \"%i\"" % episode.episodeNumbers[0]
+        addTracknum = " -track \"%i\"" % episode.episodeNumbers[0]    
     #end if episode.singleEpisode
     
-    addDisk = " --disk_n \"%i\"" % series.seasonNumber
-    addReleaseDate = " --release_date \"%s\"" % episode.firstAired
+    addDisk = " -disk \"%i\"" % series.seasonNumber
+    addReleaseDate = " -year \"%s\"" % episode.firstAired
     #addSortOrderName = " --sortOrder name \"%s\"" % episode.episodeName
     #addSortOrderArtist = " --sortOrder artist \"%s\"" % series.seriesName
     #addSortOrderAlbumArtist = " --sortOrder albumartist \"%s\"" % series.seriesName
     #addSortOrderAlbum = " --sortOrder album \"%s - Season %s\"" % (series.seriesName, series.seasonNumber)
     #addSortOrderShow = " --sortOrder show \"%s\"" % series.seriesName
-    addComment = " --comment \"tagged by mp4tvtags\""
+    addComment = " -comment \"{'imdb_id':'%s', 'tvdb_id':'%s'}\"" % (episode.imdbId, episode.id)
     
     addCast = ""
     addDirectors = ""
@@ -262,25 +260,26 @@ def tagFile(opts, program, series, episode):
     
     if len(series.actors) > 0:
         actors = createCommaSeperatedStringFromArray(series.actors)
-        addCast = " --cast \"%s\"" % actors
+        addCast = " -cast \"%s\"" % actors
     #end if len 
     if len(episode.directors) > 0:
         directors = createCommaSeperatedStringFromArray(episode.directors)
-        addDirectors = " --director \"%s\"" % directors
+        addDirectors = " -director \"%s\"" % directors
     #end if len
     if len(episode.writers) > 0:
         screenwriters = createCommaSeperatedStringFromArray(episode.writers)
-        addScreenwriters = " --screenwriters \"%s\"" % screenwriters
+        addScreenwriters = " -swriters \"%s\"" % screenwriters
     #end if len
     
     #Create the command line string
-    tagCmd = "\"%s\" -i \"%s/%s\" %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s" % \
-    (program.mp4tagger, program.dirPath, episode.fileName, addArtwork.encode("utf-8"), addMediaKind.encode("utf-8"), \
+    tagCmd = "\"%s\" %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s \"%s/%s\"" % \
+    (program.mp4tagger, addArtwork.encode("utf-8"), addMediaKind.encode("utf-8"), \
     addArtist.encode("utf-8"), addName.encode("utf-8"), addAlbum.encode("utf-8"), addGenre.encode("utf-8"), \
     addAlbumArtist.encode("utf-8"), addDescription.encode("utf-8"), addTVNetwork.encode("utf-8"), addTVShowName.encode("utf-8"), \
-    addTVEpisode.encode("utf-8"), addTVSeasonNum.encode("utf-8"), addTVEpisodeNum.encode("utf-8"), addDisk.encode("utf-8"), \
-    addTracknum.encode("utf-8"), addRating.encode("utf-8"), addReleaseDate.encode("utf-8"), addComment.encode("utf-8"), \
-    addLongDescription.encode("utf-8"), addCast.encode("utf-8"), addDirectors.encode("utf-8"), addScreenwriters.encode("utf-8"))
+    addTVEpisode.encode("utf-8"), addTVSeasonNum.encode("utf-8"), addTVEpisodeNum.encode("utf-8"), \
+    addRating.encode("utf-8"), addReleaseDate.encode("utf-8"), addComment.encode("utf-8"), \
+    addLongDescription.encode("utf-8"), addCast.encode("utf-8"), addDirectors.encode("utf-8"), addScreenwriters.encode("utf-8"), \
+    program.dirPath, episode.fileName)
     
     #run MP4Tagger using the arguments we have created
     if opts.debug:
@@ -292,12 +291,6 @@ def tagFile(opts, program, series, episode):
         print "** ERROR: %s" % result
         sys.exit(1)
     
-    lockCmd = "chflags uchg \"" + program.dirPath + "/" + episode.fileName + "\""
-    
-    os.popen(lockCmd)
-    if opts.verbose:
-        print "  Tagged and locked: " + episode.fileName
-    #end if verbose
     #end if overwrite
 #end tagFile
     
@@ -466,15 +459,13 @@ def main():
                         help="Will provide some feedback [default]")
     parser.add_option(  "-q", "--quiet", action="store_false", dest="verbose",
                         help="For ninja-like processing")
-    parser.add_option(  "-f", "--force-tagging", action="store_true", dest="forcetagging",
-                        help="Tags all valid files, even previously tagged ones")
     parser.add_option(  "-r", "--remove-tags", action="store_true", dest="removetags",
                         help="Removes all tags")
     parser.add_option(  "-n", "--no-renaming", action="store_false", dest="rename",
                         help="disables renaming")
     parser.add_option(  "-t", "--no-tagging", action="store_false", dest="tagging",
                         help="disables tagging")
-    parser.set_defaults( interactive=True, debug=False, verbose=True, forcetagging=False,
+    parser.set_defaults( interactive=True, debug=False, verbose=True,
                             removetags=False, rename=True, tagging=True )
     
     opts, args = parser.parse_args()
@@ -552,7 +543,7 @@ def main():
         if opts.removetags:
             if opts.verbose:
                 print "  Removed any pre-existing tags from %s" % episode.fileName
-            removeTagsCmd = "\"" + program.mp4tagger + "\" -i \"" + program.dirPath + "/" + episode.fileName + "\" -r -o"
+            removeTagsCmd = "\"" + program.mp4tagger + "\" -r AacCdDgGHilmMnNoPsStTywR \"" + program.dirPath + "/" + episode.fileName + "\""
             if opts.debug:
                 print "!!Remove tags command: %s" % removeTagsCmd
             #remove any pre-existing embeded artwork
